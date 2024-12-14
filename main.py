f1e336859b1e074a63cb4e068610a9462ddb0f02
Shivam869 <sanjivshivam23@gmail.com>
2024-12-14 21:51:41 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+def printnumber(n):
+    literation=0
+    literation+=1
+    print("Total literation done by the cumputer is:",literation,"\n")
+printnumber(10)
+printnumber(20)
+print("\n with any n the time taken by the computer will not change")
